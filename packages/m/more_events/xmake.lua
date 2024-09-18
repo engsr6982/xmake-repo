48f4e48a3b1bd5cc23727e7a68b0cc43ed557930
engsr6982 <pwxg286704@163.com>
2024-09-18 23:30:53 +0800

    fix: try fix link

--- packages/m/more_events/xmake.lua
+++ packages/m/more_events/xmake.lua
@@ -1,8 +1,8 @@
 package("more_events")
     add_urls("https://github.com/engsr6982/MoreEvents/releases/download/v$(version)/MoreEvents-windows-x64.zip")
-    add_versions("0.1.0","c5562979064c74e3b5b81b7feb12c2ecaed8e8deb4aa3be6528c4fc65ff424db")
+    add_versions("0.1.0","912db8cefb1eb72f1dffe8a807ef7dd06427231db3fe2d2513d4748b9471f4c0")
 
     on_install(function (package)
         os.cp("include", package:installdir())
-        os.cp("lib/**/*.lib", package:installdir("lib"))
+        os.cp("lib/*.lib", package:installdir("lib"))
     end)
