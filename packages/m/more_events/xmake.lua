31b3a422f40beaba19ab31e0214a7bcdeb445285
engsr6982 <pwxg286704@163.com>
2024-09-21 11:51:25 +0800

    chore: update repo url

--- packages/m/more_events/xmake.lua
+++ packages/m/more_events/xmake.lua
@@ -1,5 +1,8 @@
 package("more_events")
-    add_urls("https://github.com/engsr6982/MoreEvents/releases/download/v$(version)/MoreEvents-windows-x64.zip")
+    set_homepage("https://github.com/engsr6982/MoreEvents")
+    set_license("GPL-3.0")
+
+    add_urls("https://github.com/engsr6982/MoreEvents/releases/download/v$(version)/MoreEvents-windows-x64.zip", "https://github.com/engsr6982/MoreEvents.git")
     add_versions("0.1.0","912db8cefb1eb72f1dffe8a807ef7dd06427231db3fe2d2513d4748b9471f4c0")
 
     on_install(function (package)
