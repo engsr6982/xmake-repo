fd7835f320e17dbcba525b36589cc552ec7dccbb
engsr6982 <109733049+engsr6982@users.noreply.github.com>
2024-10-09 10:52:20 +0800

    📦(pland): update v0.1.0

--- packages/p/pland/xmake.lua
+++ packages/p/pland/xmake.lua
@@ -2,7 +2,8 @@ package("pland")
     set_homepage("https://github.com/engsr6982/PLand")
 
     add_urls("https://github.com/engsr6982/PLand/releases/download/v$(version)/SDK-PLand.zip", "https://github.com/engsr6982/PLand.git")
-    add_versions("0.0.3","0e63d412242e61f20194fb3a47541e39bf42c0a04ee38edc135c868cf9d8c13d")
+    add_versions("0.0.3", "0e63d412242e61f20194fb3a47541e39bf42c0a04ee38edc135c868cf9d8c13d")
+    add_versions("0.1.0", "313aeaea5296f3bdfb514f96a1d11a3d661408897d3beb8d18f3fda2afd591ae")
 
     on_install(function (package)
         os.cp("include", package:installdir())
