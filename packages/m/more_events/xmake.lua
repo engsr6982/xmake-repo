667b73f1ea4cf52aadc191bbe0591a13852d6c95
engsr6982 <109733049+engsr6982@users.noreply.github.com>
2024-10-09 10:54:37 +0800

    📦(MoreEvents): update v0.2.1

--- packages/m/more_events/xmake.lua
+++ packages/m/more_events/xmake.lua
@@ -7,6 +7,7 @@ package("more_events")
     -- add_versions("0.1.1","a699f1bec06947e9431494fd3ec9c23f6a3c1358f27f71fa8e3daaa328629e34")
     add_versions("0.1.2","37deb0c6036bbf363c21673f9a9ed2be5d17c8d76362c794f3c931e6550bbfda")
     add_versions("0.2.0","89d71fbbf82a5c1ee369be7b3d45b71e6bd1b67e5d60fb5105032e541d819138")
+    add_versions("0.2.1", "8a87d11315394fa40acbf1a518dce2424842492d0b2c7fc51d280f7e929fd660")
 
     on_install(function (package)
         os.cp("include", package:installdir())
