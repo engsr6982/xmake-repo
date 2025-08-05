d679ce43747b49770ce4fa05c9efe7d6c69e7ff6
engsr6982 <pwxg286704@163.com>
2025-08-05 15:26:14 +0800

    feat(teleportsystem): add xmake.lua for TeleportSystem package

---
+++ packages/t/teleportsystem/xmake.lua
@@ -0,0 +1,14 @@
+package("teleportsystem")
+    set_homepage("https://github.com/engsr6982/TeleportSystem")
+
+    -- <Version, CommitHash>
+    add_urls("https://github.com/engsr6982/TeleportSystem.git")
+    add_versions("0.12.0", "ff515c84c56c9fca440ce8ccf89c4d8e2ace5460")
+
+    on_install(function (package)
+        import("core.base.semver")
+        cprint(
+            "${bright green}info: ${clear}Downloading PLand SDK for release: ${bright cyan}" .. version .. "${clear}"
+        )
+        import("package.tools.xmake").install(package)
+    end)
