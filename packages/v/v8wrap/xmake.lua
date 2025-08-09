bb1cc49e23ecd3243c74429e129fcc66fa82afe5
engsr6982 <pwxg286704@163.com>
2025-08-09 21:54:46 +0800

    build(v8wrap): add version 0.1.1

--- packages/v/v8wrap/xmake.lua
+++ packages/v/v8wrap/xmake.lua
@@ -5,6 +5,7 @@ package("v8wrap")
     -- <Version, CommitHash>
     add_urls("https://github.com/engsr6982/v8wrap.git")
     add_versions("0.1.0", "0e1e780c7df72a55fd3619653a99cfe230d67c41")
+    add_versions("0.1.1", "848be4feb7e750005f5822570dad85e4ddd85e08")
 
     on_install(function (package)
         import("package.tools.xmake").install(package)
