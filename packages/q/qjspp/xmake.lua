03e955fe60c327acca601d3e8df5163e78791896
engsr6982 <pwxg286704@163.com>
2025-08-20 21:14:50 +0800

    build: add qjspp version 0.2.0 and update quickjs-ng dependency

--- packages/q/qjspp/xmake.lua
+++ packages/q/qjspp/xmake.lua
@@ -5,6 +5,11 @@ package("qjspp")
     -- <Version, CommitHash>
     add_urls("https://github.com/engsr6982/qjspp.git")
     add_versions("0.1.0", "d322af3e4fe397654664cbc02a1708b44c738977")
+    add_versions("0.2.0", "8103382d1512d52aabca894353e7fe274a6ebc2e")
+
+    on_load(function (package)
+        package:add("deps", "quickjs-ng 3c9afc9943323ee9c7dbd123c0cd991448f4b6c2", { configs={ libc=true } })
+    end)
 
     on_install(function (package)
         import("package.tools.xmake").install(package)
