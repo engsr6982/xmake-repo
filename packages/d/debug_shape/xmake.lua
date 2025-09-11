e86f6199cc4c2969d4ab9af3f2619eaa7fac9205
engsr6982 <pwxg286704@163.com>
2025-09-11 16:22:03 +0800

    build: add version 0.2.0 for debug_shape package

--- packages/d/debug_shape/xmake.lua
+++ packages/d/debug_shape/xmake.lua
@@ -5,6 +5,7 @@ package("debug_shape")
     -- <Version, CommitHash>
     add_urls("https://github.com/engsr6982/DebugShape.git")
     add_versions("0.1.0", "5b5e740fa130985bc14c615836e2a0dda9a97e7c")
+    add_versions("0.2.0", "7c337b5fd6b0c55c03010f535697655ee3d477b5")
 
     on_install(function (package)
         import("package.tools.xmake").install(package)
