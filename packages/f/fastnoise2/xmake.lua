0646a9ddd91bc961a8660166af8ce65bc52f5833
engsr6982 <pwxg286704@163.com>
2025-09-29 23:49:49 +0800

    build: add fastnoise2 package

---
+++ packages/f/fastnoise2/xmake.lua
@@ -0,0 +1,22 @@
+package("fastnoise2")
+    set_license("MIT")
+    set_homepage("https://github.com/Auburn/FastNoise2")
+    add_urls("https://github.com/Auburn/FastNoise2.git")
+
+    -- <Version, CommitHash>
+    add_versions("v0.10.0-alpha", "9b75083b87d5e391d43dc0c721d8a9fa51db735a")
+
+
+    add_includedirs("include")
+
+    add_deps("cmake")
+
+    on_install(function (package)
+        local configs = {
+            "-DFASTNOISE2_TESTS=OFF",
+            "-DFASTNOISE2_NOISETOOL=OFF"
+        }
+        table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:debug() and "Debug" or "Release"))
+        table.insert(configs, "-DBUILD_SHARED_LIBS=" .. (package:config("shared") and "ON" or "OFF"))
+        import("package.tools.cmake").install(package, configs)
+    end)
